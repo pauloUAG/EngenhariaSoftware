5d1142b7ec6ccb1857fe0c759a5f3222fb39b0e6
Rodrigo Andrade <rodrigo_cardoso@hotmail.it>
2016-01-19 15:44:49 -0300

    implementing auth mechanism with shiro plugin

---
+++ grails-app/domain/br/ufrpe/uag/ShiroRole.groovy
@@ -0,0 +1,12 @@
+package br.ufrpe.uag
+
+class ShiroRole {
+    String name
+
+    static hasMany = [ users: ShiroUser, permissions: String ]
+    static belongsTo = ShiroUser
+
+    static constraints = {
+        name(nullable: false, blank: false, unique: true)
+    }
+}
